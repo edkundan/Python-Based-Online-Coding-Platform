9ad29d9dc61260f99e95de28b57dc65218aabcf7
Kundan Kumar <74104870+edkundan@users.noreply.github.com>
2024-11-29 14:43:51 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,34 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Fetch all users from the database using UserDAO
+        UserDAO userDAO = new UserDAO();
+        System.out.println("Fetching all users:");
+        userDAO.getAllUsers();  // Fetch all users
+        
+        // Fetch all code executions from the database using CodeExecutionDAO
+        CodeExecutionDAO executionDAO = new CodeExecutionDAO();
+        System.out.println("Fetching all code executions:");
+        executionDAO.getAllExecutions();  // Fetch all code executions
+        
+        // Fetch all challenges from the database using ChallengeDAO
+        ChallengeDAO challengeDAO = new ChallengeDAO();
+        System.out.println("Fetching all challenges:");
+        challengeDAO.getAllChallenges();  // Fetch all challenges
+        
+        // Fetch all submissions from the database using SubmissionDAO
+        SubmissionDAO submissionDAO = new SubmissionDAO();
+        System.out.println("Fetching all submissions:");
+        submissionDAO.getAllSubmissions();  // Fetch all submissions
+        
+        // You can also directly query the 'users' table in the database using JDBC
         String query = "SELECT * FROM users";
         
         try (Connection conn = DBConnection.getConnection();
              Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(query)) {
 
+            System.out.println("Fetching users directly via JDBC:");
             while (rs.next()) {
                 int userId = rs.getInt("id");
                 String username = rs.getString("username");
